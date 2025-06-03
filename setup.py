8c33e9254723f980b8598a52a6c58dd29109c1f4
Claas Nieslony <github@nieslony.at>
2025-06-03 14:24:22 +0200

    Build RPMs from spec file

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+# import src.arachne_dbus as arachne_dbus
+
+setup(
+    name = "pyarachnecdl",
+    description = "Arachne Config Downloader",
+    version = "0.1",
+    author = "Claas Nieslony",
+    license = "GPLv3",
+    packages = ["pyarachnecdl"],
+    package_dir = {
+        "": "src"
+        },
+    entry_points = {
+        'console_scripts': [
+            'pyarachnecdl = pyarachnecdl.arachne_config_downloader:main',
+        ],
+    },
+    package_data={
+        "pyarachnecdl": ["data/*svg"],
+    },
+    install_requires=[
+          "pyqt6",
+          "requests",
+          "requests-kerberos"
+    ],
+)
